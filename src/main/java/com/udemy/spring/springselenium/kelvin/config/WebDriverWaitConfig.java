83cc53119d26c710134121afb77fd75b73937760
Vinoth Selvaraj <vino.infy@gmail.com>
2020-05-21 17:31:45 -0500

    webdriverwait scope

--- src/main/java/com/udemy/spring/springselenium/kelvin/config/WebDriverWaitConfig.java
+++ src/main/java/com/udemy/spring/springselenium/kelvin/config/WebDriverWaitConfig.java
@@ -5,7 +5,9 @@ import com.udemy.spring.springselenium.kelvin.annotation.LazyConfiguration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @LazyConfiguration
 public class WebDriverWaitConfig {
@@ -14,6 +16,7 @@ public class WebDriverWaitConfig {
     private int timeout;
 
     @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public WebDriverWait webdriverWait(WebDriver driver){
         return new WebDriverWait(driver, this.timeout);
     }
