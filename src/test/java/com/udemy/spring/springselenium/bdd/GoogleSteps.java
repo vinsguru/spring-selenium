274d2dcbcb36b8f3ff0289af305721f2d22c5e52
Vinoth Selvaraj <vino.infy@gmail.com>
2020-05-25 09:54:33 -0500

    bdd final with google steps

---
+++ src/test/java/com/udemy/spring/springselenium/bdd/GoogleSteps.java
@@ -0,0 +1,37 @@
+package com.udemy.spring.springselenium.bdd;
+
+import com.udemy.spring.springselenium.kelvin.annotation.LazyAutowired;
+import com.udemy.spring.springselenium.page.google.GooglePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
+
+@SpringBootTest
+public class GoogleSteps {
+
+    @LazyAutowired
+    private GooglePage googlePage;
+
+    @Given("I am on the google site")
+    public void launchSite() {
+        this.googlePage.goTo();
+    }
+
+    @When("I enter {string} as a keyword")
+    public void enterKeyword(String keyword) {
+        this.googlePage.getSearchComponent().search(keyword);
+    }
+
+    @Then("I should see search results page")
+    public void clickSearch() {
+        Assert.assertTrue(this.googlePage.getSearchResult().isAt());
+    }
+
+    @Then("I should see at least {int} results")
+    public void verifyResults(int count) {
+        Assert.assertTrue(this.googlePage.getSearchResult().getCount() >= count);
+    }
+
+}
